c1240cbba8384e7cb827af06c76641148c0a2860
helcorin <helcorintho@gmail.com>
2020-10-14 00:56:10 +0000

    Update resource

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
 	ami		= "ami-0dba2cb6798deb6d8"
 	instance_type	= "t2.micro"
+	subnet_id	= "subnet-00c345430671ed485"
+        associate_public_ip_address = true
+        vpc_security_group_ids = ["sg-0f832f987be4f0b1e"]
 
 	tags = {
 		project	= "demo"
